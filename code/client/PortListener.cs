19a61b7dfd0bf30ba3bb300882ae0fe4d7c15373
MistakeNot4892 <AGMogett@gmail.com>
2022-01-05 08:58:16 +1100

    Added exits command.

--- code/client/PortListener.cs
+++ code/client/PortListener.cs
@@ -1,7 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
-using System.Diagnostics;
 
 namespace inspiral
 {
---
+++ code/commands/general/CommandExits.cs
@@ -0,0 +1,27 @@
+namespace inspiral
+{
+	internal class CommandExits : GameCommand
+	{
+		internal override void Initialize()
+		{
+			aliases = new System.Collections.Generic.List<string>() { "exits" };
+			description = "Shows the exits of the current location.";
+			usage = "exits";
+		}
+		internal override void InvokeCommand(GameObject invoker, CommandData cmd)
+		{
+			if(invoker.location == null || !invoker.location.HasComponent<RoomComponent>())
+			{
+				invoker.SendLine("You cannot see any exits here.");
+				invoker.SendPrompt(); 
+				return;
+			}
+			RoomComponent roomComp = (RoomComponent)invoker.location.GetComponent<RoomComponent>();
+			invoker.WriteLine($"{Colours.Fg(roomComp.GetExitString(), Colours.BoldCyan)}");
+			invoker.SendPrompt(); 
+		}
+	}
+}
+
+
+
